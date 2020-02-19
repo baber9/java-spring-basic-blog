ed69bc187f4209f08fa6ccd607d10ec660ef2d30
BABER-DELL\baber <baber9@gmail.com>
2020-02-18 22:41:53 -0600

    updates

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,26 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
-    @RequestMapping("/")
-    public String listPosts(ModelMap modelMap) {
-        modelMap.put("title", "Blog Post 1");
-        return "home";
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepo){
+        postRepository = postRepo;
     }
 
+    @RequestMapping("/")
+    public String listPosts(ModelMap modelMap) {
+        List<Post> posts = postRepository.getAllPosts();
+        modelMap.put("posts", posts);
+        return "home";
+    }
 }
